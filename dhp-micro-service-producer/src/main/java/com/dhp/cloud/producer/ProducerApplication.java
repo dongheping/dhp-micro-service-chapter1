db2b54ddbfb908c07b29961d0d713832f55e28c5
donghp <dhp_work@163.com>
2019-12-23 22:05:25 +0800

    init project

---
+++ dhp-micro-service-producer/src/main/java/com/dhp/cloud/producer/ProducerApplication.java
@@ -0,0 +1,20 @@
+package com.dhp.cloud.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @ClassName ProducerApplication
+ * @Description 生产者启动类
+ * @Author DongHP
+ * @Date 2019-12-22
+ */
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+}
